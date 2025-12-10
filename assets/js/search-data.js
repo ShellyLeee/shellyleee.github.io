31987618ac0219dfaa1362ce6b76dc9481f5d0e7
ShellyLeee <shellyli1105@gmail.com>
2025-12-10 09:15:25 +0000

    Deploying to gh-pages from @ ShellyLeee/shellyleee.github.io@0f4fedd8bde2478ad1d5d79ae27ddb90b6080fe9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,17 +52,6 @@ ninja.data = [{
             window.location.href = "/blog/2025/plotly/";
           
         },
-      },{id: "post-interpretable-generative-models-through-post-hoc-concept-bottlenecks-cvpr-2025",
-        
-          title: 'Interpretable Generative Models through Post-hoc Concept Bottlenecks [CVPR 2025] <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "A blog-style walkthrough of our CVPR 2025 paper on post-hoc concept bottleneck models and their interpretability.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@yit020/interpretable-generative-models-through-post-hoc-concept-bottlenecks-cvpr-2025-64b55acd19cd", "_blank");
-          
-        },
       },{id: "post-a-post-with-image-galleries",
         
           title: "a post with image galleries",
