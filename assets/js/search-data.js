0fd427834cf6f03d7909fb43a21d7286275f8bdd
ShellyLeee <shellyli1105@gmail.com>
2025-12-10 04:23:26 +0000

    Deploying to gh-pages from @ ShellyLeee/shellyleee.github.io@b45770f51e2a09607999589c362b5b4e51874f6f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -60,9 +60,9 @@ ninja.data = [{
             },},{id: "news-i-create-my-personal-website",
           title: 'I create my personal website!',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
