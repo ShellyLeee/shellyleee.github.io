3918267768fb391065c8c11e0e885ceb80c052f7
ShellyLeee <shellyli1105@gmail.com>
2025-12-10 10:00:32 +0000

    Deploying to gh-pages from @ ShellyLeee/shellyleee.github.io@f7361e0780bdd055c8fa5d5755e52ffb34506aa2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,29 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/assets/pdf/Yixuan_Li_CV.pdf";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-uncertainty-estimation-methods-in-large-language-models-a-taxonomy",
+        
+          title: "Uncertainty Estimation Methods in Large Language Models - A Taxonomy",
+        
+        description: "A systematic taxonomy of uncertainty estimation methods for Large Language Models, categorizing key literature from token-level probabilities to semantic clustering and internal state probing.",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/UQ/";
+          
+        },
+      },{id: "post-reproduction-and-extension-interpretable-generative-models-through-post-hoc-concept-bottlenecks-cvpr-2025",
+        
+          title: 'Reproduction and Extension - Interpretable Generative Models through Post-hoc Concept Bottlenecks (CVPR 2025)... <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "A blog-style walkthrough of the CVPR 2025 paper on post-hoc concept bottleneck models and their interpretability.",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://medium.com/@yit020/interpretable-generative-models-through-post-hoc-concept-bottlenecks-cvpr-2025-64b55acd19cd", "_blank");
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
